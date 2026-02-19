3d3e3978cab3ce6f72e0acd0e06c426a46b6da94
vanstrong4 <alfeushendrievan@gmail.com>
2026-02-19 17:04:20 +0700

    membuat logic buah jatoh

--- lib/game/components/fruit.dart
+++ lib/game/components/fruit.dart
@@ -6,8 +6,31 @@ import 'package:flutter/material.dart';
 import '../fruit_cacther_game.dart';
 import 'basket.dart';
 
-
 enum FruitType { apple, banana, orange, strawberry }
-class Fruit extends PositionComponent with HasGameRef<FruitCatcherGame>, CollisionCallbacks {
-  
+
+class Fruit extends PositionComponent
+  with HasGameRef<FruitCatcherGame>, CollisionCallbacks {
+  final FruitType type;
+  final double fallSpeed = 200;
+  final Random random = Random();
+  Fruit({super.position})
+    : type = FruitType.values[Random().nextInt(FruitType.values.length)],
+      super(size: Vector2.all(40));
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    anchor = Anchor.center;
+    add(CircleHitbox());
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    position.y += fallSpeed * dt;
+
+    if (position.y > gameRef.size.y + 50) {
+      removeFromParent();
+    }
+  }
 }
