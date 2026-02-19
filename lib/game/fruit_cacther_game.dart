e2da884d4f21beed183aecccaaa0e8559aecf6d0
vanstrong4 <alfeushendrievan@gmail.com>
2026-02-19 15:50:45 +0700

    buat valuenotifier dan background kuning

--- lib/game/fruit_cacther_game.dart
+++ lib/game/fruit_cacther_game.dart
@@ -1,5 +1,12 @@
 import 'package:flame/game.dart';
+import 'package:flutter/widgets.dart';
 
 class FruitCatcherGame extends FlameGame {
-
+  @override
+  Color backgroundColor() => const Color.fromARGB(255, 247, 225, 152);
+  final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+  }
 }
