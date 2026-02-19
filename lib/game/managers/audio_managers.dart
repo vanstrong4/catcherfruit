c1a53f675a12f9e43a3d3dd78b19bf59f1fe8cee
vanstrong4 <alfeushendrievan@gmail.com>
2026-02-19 16:49:20 +0700

    buat singleton dan getters

--- lib/game/managers/audio_managers.dart
+++ lib/game/managers/audio_managers.dart
@@ -1,3 +1,14 @@
-class AudioManagers {
-  
+class AudioManager {
+  static final AudioManager _instance = AudioManager._internal();
+  factory AudioManager() => _instance;
+  AudioManager._internal();
+  bool _isMusicEnabled = true;
+  bool _isSfxEnabled = true;
+  double _musicVolume = 0.7;
+  double _sfxVolume = 1.0;
+
+  bool get isMusicEnabled => _isMusicEnabled;
+  bool get isSfxEnabled => _isSfxEnabled;
+  double get musicVolume => _musicVolume;
+  double get sfxVolume => _sfxVolume;
 }
