209780d7a218c298ff7a0e4ab5525b295a0d81c4
vanstrong4 <alfeushendrievan@gmail.com>
2026-02-19 16:51:25 +0700

    inisialisasi music dan sfx

--- lib/game/managers/audio_managers.dart
+++ lib/game/managers/audio_managers.dart
@@ -1,3 +1,5 @@
+import 'package:flame_audio/flame_audio.dart';
+
 class AudioManager {
   static final AudioManager _instance = AudioManager._internal();
   factory AudioManager() => _instance;
@@ -11,4 +13,23 @@ class AudioManager {
   bool get isSfxEnabled => _isSfxEnabled;
   double get musicVolume => _musicVolume;
   double get sfxVolume => _sfxVolume;
+
+
+  Future<void> initialize() async {
+  try {
+    
+  await FlameAudio.audioCache.loadAll([
+    'music/xtremefreddy-game-music-loop-7-145285.mp3',
+    'sfx/bunny_cube98-apple-bite-316785.mp3',
+    'sfx/explosion.mp3',
+    'sfx/jump.mp3',
+  ]);
+
+  print('Audio initialized successfully');
+
+  } catch (e) {
+    print('Error initializing audio: $e');
+  }
+  }
+
 }
