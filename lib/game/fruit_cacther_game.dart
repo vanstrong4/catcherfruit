38586b888872ddcf6d70805e954293e2750fac61
vanstrong4 <alfeushendrievan@gmail.com>
2026-02-19 17:10:08 +0700

    update fruit catcher game

--- lib/game/fruit_cacther_game.dart
+++ lib/game/fruit_cacther_game.dart
@@ -1,14 +1,79 @@
-import 'package:catcherfruit/game/managers/audio_managers.dart';
+import 'dart:math';
+import 'package:flame/camera.dart';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'components/basket.dart';
+import 'components/fruit.dart';
+import 'managers/audio_managers.dart';
 
-class FruitCatcherGame extends FlameGame {
+class FruitCatcherGame extends FlameGame
+    with PanDetector, HasCollisionDetection {
+  late Basket basket;
+  late TextComponent scoreText;
+  final Random random = Random();
+  double fruitSpawnTimer = 0;
+  final double fruitSpawnInterval = 1.5;
+
   @override
   Color backgroundColor() => const Color.fromARGB(255, 247, 225, 152);
   final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
+  int _score = 0;
+
+  int get score => _score;
+  set score(int value) {
+    _score = value;
+    scoreNotifier.value = value;
+  }
+
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+    camera.viewport = FixedResolutionViewport(resolution: Vector2(400, 800));
+    basket = Basket();
+    await add(basket);
     AudioManager().playBackgroundMusic();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    fruitSpawnTimer += dt;
+    if (fruitSpawnTimer >= fruitSpawnInterval) {
+      spawnFruit();
+      fruitSpawnTimer = 0;
+    }
+  }
+
+  void spawnFruit() {
+    final x = random.nextDouble() * size.x;
+    final fruit = Fruit(position: Vector2(x, -50));
+    add(fruit);
+  }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    basket.position.x += info.delta.global.x;
+    basket.position.x = basket.position.x.clamp(
+      basket.size.x / 2,
+      size.x - basket.size.x / 2,
+    );
+  }
+
+  void incrementScore() {
+    score++;
+    AudioManager().playSfx('bunny_cube98-apple-bite-316785.mp3');
+  }
+
+  void gameOver() {
+    AudioManager().playSfx('explosion.mp3');
+    pauseEngine();
+  }
+
+  @override
+  void onRemove() {
+    AudioManager().stopBackgroundMusic();
+    super.onRemove();
   }
 }
